b82cc0bbbbf1a22851ca03f336be434799050381
Yojit <yojitj940@gmail.com>
2026-07-24 13:46:15 +0530

    Time: 0 ms (100.00%) | Memory: 28.4 MB (11.87%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
+        //Boyer - Moore Voting Algorithm
         int n = nums.size();
         if (n == 1) return nums[0];
-        
-        int req = n / 2;
-        map<int, int> m;
-        for (int i = 0; i < n; i++){
-            m[nums[i]]++;
+        int maj = nums[0];
+        int count = 1;
+        for (int i = 1; i < n; i++){
+            if (count == 0){
+                maj = nums[i];
+            }
+            if (nums[i] == maj){
+                count++;
+            }else{
+                count--;
+            }
         }
-        
-        int max_val = INT_MIN;
-        int max_key;
-        for (const auto &i: m){
-            if ((i.second > max_val) && (i.second > req)){
-                max_val = i.second;
-                max_key = i.first;
-            }      
-        }
-        return max_key;
+        return maj;
     }
 };
