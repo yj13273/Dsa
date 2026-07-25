9eb230f2ec0e17b8ef71d6c36e4d93cbb67f6498
Yojit <yojitj940@gmail.com>
2026-07-25 10:55:50 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (49.39%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -2,34 +2,25 @@ class Solution {
 public:
     void sortColors(vector<int>& nums) {
         int n = nums.size();
-        int cnt0 = 0;
-        int cnt1 = 0;
-        int cnt2 = 0;
+        int write = 0;
         for (int i = 0; i < n; i++){
             if (nums[i] == 0){
-                cnt0++;
-            }else if(nums[i] == 1){
-                cnt1++;
-            }else if(nums[i] == 2){
-                cnt2++;
+                swap(nums[write], nums[i]);
+                write++;
             }
         }
-        cout << cnt0 << " " << cnt1 << " " << cnt2;
-        int j = 0;
-        while(cnt0 > 0){
-            nums[j] = 0;
-            cnt0--;
-            j++;
-        }   
-        while(cnt1 > 0){
-            nums[j] = 1;
-            cnt1--;
-            j++;
-        }  
-        while(cnt2 > 0){
-            nums[j] = 2;
-            cnt2--;
-            j++;
-        }  
+        for (int i = 0; i < n; i++){
+            if (nums[i] == 1){
+                swap(nums[write], nums[i]);
+                write++;
+            }
+        }
+        for (int i = 0; i < n; i++){
+            if (nums[i] == 2){
+                swap(nums[write], nums[i]);
+                write++;
+            }
+        }
+        
     }
 };
