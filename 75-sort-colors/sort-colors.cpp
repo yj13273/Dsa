184e21de74764e8e0e3a617e7ca9bda0fe39ac53
Yojit <yojitj940@gmail.com>
2026-07-25 11:13:43 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (49.39%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,26 +1,20 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
+        //Optimzation by removing redunant passing
         int n = nums.size();
         int write = 0;
-        for (int i = 0; i < n; i++){
-            if (nums[i] == 0){
-                swap(nums[write], nums[i]);
-                write++;
+        int key = 0;
+        //i range before: 0,1,2
+        //Processing 2 is unnecessary so i = 0,1 is sufficient
+        for (int i = 0; i < 2; i++){
+            for (int i = write; i < n; i++){
+                if (nums[i] == key){
+                    swap(nums[write], nums[i]);
+                    write++;
+                }
             }
+            key++;
         }
-        for (int i = 0; i < n; i++){
-            if (nums[i] == 1){
-                swap(nums[write], nums[i]);
-                write++;
-            }
-        }
-        for (int i = 0; i < n; i++){
-            if (nums[i] == 2){
-                swap(nums[write], nums[i]);
-                write++;
-            }
-        }
-        
     }
 };
