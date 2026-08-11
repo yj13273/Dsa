e276b2b3b8c7289f6e90b450e4d6f32d86d6bdb9
Yojit <yojitj940@gmail.com>
2026-08-11 09:11:44 +0530

    Time: 12 ms (21.83%) | Memory: 128.3 MB (54.22%) - LeetSync

---
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> v(n);
+        int k = 0;
+        for(int i = 0; i < n; i++){
+            if (nums[i] > 0){
+                v[k] = nums[i];
+                k += 2;
+            }
+        }
+        k = 1;
+        for(int i = 0; i < n; i++){
+            if (nums[i] < 0){
+                v[k] = nums[i];
+                k += 2;
+            }
+        }
+        return v;
+    }
+};
